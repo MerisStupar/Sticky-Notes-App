6a3e5f635817f9ca607cce0de378722382e97e7d
WendigoM3ris <merisstupar11@gmail.com>
2021-10-08 00:51:57 +0200

    Enabled button to add notes, edit note and deleting note on double click

--- src/main.js
+++ src/main.js
@@ -6,6 +6,10 @@ getNotes().forEach(note=>{
     notesContainer.insertBefore(noteElement, addNoteButton);
 });
 
+addNoteButton.addEventListener("click",()=>{
+    addNote();
+});
+
 function getNotes() {
     return JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
 }
@@ -38,15 +42,30 @@ function createNoteElement(id, content) {
 }
 
 function addNote() {
-    
+    const notes = getNotes();
+    const noteObject = {
+        id : Math.floor(Math.random() * 100000),
+        content: ""
+    };
+    const noteElement = createNoteElement(noteObject.id, noteObject.content);
+    notesContainer.insertBefore(noteElement, addNoteButton);
+
+    notes.push(noteObject);
+    saveNotes(notes);
 }
 
 function updateNote(id, newContent) {
-    console.log("Updating Note...");
-    console.log(id, newContent);
+    const notes = getNotes();
+    const targetNote = notes.filter(note => note.id == id) [0];
+
+    targetNote.content = newContent;
+    saveNotes(notes);
+
 }
 
 function deleteNote(id, element) {
-    console.log("Deleting Note...");
-    console.log(id);
+    const notes = getNotes().filter(note => note.id != id);
+
+    saveNotes(notes);
+    notesContainer.removeChild(element);
 }
